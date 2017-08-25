b6f0a1e1f2bf299c7700210b91921dafef204d1e
Thanos Korakas <thanoskorakas@gmail.com>
2017-08-25 22:53:12 +0300

    Comments

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Todo DB.
 var todos = [];
 
+// Get all todos.
 app.get('/', (req, res) => {
     res.send(todos)
 });
 
+// Create todo.
 app.post('/', (req, res) => {
     todo = {
         id: uuidv4(),
@@ -23,22 +26,28 @@ app.post('/', (req, res) => {
     res.send(todo);
 });
 
+// Mark todo as done
+// @param: id
 app.patch('/', (req, res) => {
     let todo = todos.filter(todo => todo.id === req.body.id);
     todo[0].done = !todo[0].done;
     res.send(todo[0]);
 });
 
+// Delete a todo
+// @param: id
 app.delete('/', (req, res) => {
     todos = todos.filter(todo => todo.id !== req.body.id);
     res.send(todos)
 });
 
+// Clear all completed todos.
 app.get('/clear', (req, res) => {
     todos = todos.filter(todo => !todo.done);
     res.send(todos)
 });
 
+// Start server.
 app.listen(3000, function () {
     console.log('React series backend listening on port 3000!')
 });
